e7cb227b32f831d1f64c56b5a0d992be45377fe6
Luis Perez <ljpm9401@gmail.com>
2022-06-26 17:21:50 -0500

    Actividades didacticas

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -17,7 +17,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Completar
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad1/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/actividad1/index.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
 
@@ -35,7 +35,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Selección única
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad2/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/actividad2/index.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
 
